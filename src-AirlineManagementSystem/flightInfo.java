d6acddf3155e1baad15ded89c586c06668c1b4eb
TanmayPathak19 <tanmaypathak153@gmail.com>
2024-12-19 01:33:52 +0530

    Update flightInfo.java

--- src-AirlineManagementSystem/flightInfo.java
+++ src-AirlineManagementSystem/flightInfo.java
@@ -1,13 +1,28 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
-package airline.management.system;
+package airlinemanagementsystem;
 
-/**
- *
- * @author SAKSHI CHOUDHARY
- */
-public class flightInfo {
-    
+import javax.swing.*;
+import java.awt.*;
+
+public class FlightInfo extends JFrame {
+
+    public FlightInfo() {
+        // Setting background color for aesthetics
+        getContentPane().setBackground(Color.WHITE);
+        setLayout(null);
+
+        // Adding a scrollable table
+        JTable table = new JTable();
+        JScrollPane jsp = new JScrollPane(table);
+        jsp.setBounds(0, 0, 800, 500);
+        add(jsp);
+
+        // Window settings for responsiveness
+        setSize(800, 500);
+        setLocation(400, 200);
+        setVisible(true);
+    }
+
+    public static void main(String[] args) {
+        new FlightInfo();
+    }
 }
